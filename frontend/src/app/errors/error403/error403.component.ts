062a3b1165819f3733e4320417ce9f232b21b1d0
Ismaelgzse <ismaelgzse@gmail.com>
2023-06-13 14:26:50 +0200

    Adding error components

---
+++ frontend/src/app/errors/error403/error403.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  templateUrl: 'error403.component.html',
+  styleUrls: ['../../../assets/css/home.css','../../../assets/css/errors.css', '../../../assets/vendor/fontawesome-free-6.4.0-web/css/all.css']
+})
+
+export class Error403Component implements OnInit{
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+
+}
+
+
---
+++ frontend/src/app/errors/error404/error404.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  templateUrl: 'error404.component.html',
+  styleUrls: ['../../../assets/css/home.css','../../../assets/css/errors.css', '../../../assets/vendor/fontawesome-free-6.4.0-web/css/all.css']
+})
+
+export class Error404Component implements OnInit{
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
