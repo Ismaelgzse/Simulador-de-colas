ab46eddb8f5111790eaedab1cd99902b88a7604a
Ismaelgzse <ismaelgzse@gmail.com>
2023-06-13 14:26:59 +0200

    Create error500.component.ts

---
+++ frontend/src/app/errors/error500/error500.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  templateUrl: 'error500.component.html',
+  styleUrls: ['../../../assets/css/home.css','../../../assets/css/errors.css', '../../../assets/vendor/fontawesome-free-6.4.0-web/css/all.css']
+})
+
+export class Error500Component implements OnInit{
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
