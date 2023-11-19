166ab1e5d3a8bb2a4a47f962c29035cc3236a0cd
Ismaelgzse <ismaelgzse@gmail.com>
2023-11-19 20:18:36 +0100

    Update SPAController.java

--- Backend/src/main/java/es/tfg/simuladorteoriacolas/SPA/SPAController.java
+++ Backend/src/main/java/es/tfg/simuladorteoriacolas/SPA/SPAController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SPAController {
 
-    @GetMapping({"/app/**/{path:[^\\.]*}","/{path:app[^\\.]*}"})
+    @GetMapping({"/app/**/{path:[^\\.]*}","/{path:app[^\\.]*}","/app/"})
     public String redirect(){
         return "forward:/app/index.html";
     }
